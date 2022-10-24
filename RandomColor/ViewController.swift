4b944db34714d8438aa4c366661170a265ab27b7
buraksahinerden <burak.erden@getir.com>
2022-10-24 15:45:59 +0300

    Initial Commit

---
+++ RandomColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColor
+//
+//  Created by Burak Şahin Erden on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
